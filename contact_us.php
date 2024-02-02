cfd2dc27100c22d5be315d1057b63e65320faeee
SRIDHAR S <149185042+sridharkpr45@users.noreply.github.com>
2024-02-02 18:24:43 +0530

    Update contact_us.php

--- contact_us.php
+++ contact_us.php
@@ -3,20 +3,17 @@
     $db_username="root";
     $db_password="";
     $db_name="tour";
-
     $conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
     if(!$conn)
     {
         echo"Connection Failed:",mysqli_connect_error();
         exit;
     }
-
     $name=$_POST['name'];
     $email=$_POST['email'];
     $phone=$_POST['phone'];
     $subject=$_POST['subject'];
     $message=$_POST['message'];
-
     $sql="Insert into contact(Name,Email,Phone,Subject,Message) values ('$name','$email','$phone','$subject','$message')";
     $result=mysqli_query($conn,$sql);
     if(!$result)
@@ -26,5 +23,4 @@
     }
     echo "We will contact you soon";
     mysqli_close($conn);
- 
 ?>
